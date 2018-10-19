450ee6ecad635905242a79b1e817190656c416a3
Alan Wong <captainalan@gmail.com>
2018-10-19 11:30:18 -0700

    Cleaning up the code, getting rid of magic numbers, adding some functional programming things

--- npc_maker.js
+++ npc_maker.js
@@ -1,9 +1,22 @@
 elm = document.getElementById("npc_maker");
 
-const HEAD  = ['plain','cowboy_hat', 'cat_ears',];
-const EYES  = ['plain','glowing','glasses',];
-const LOWER = ['plain','bandana','bane_mask',];
+const WARDROBE = {
+  head:['plain','cowboy_hat', 'cat_ears',],
+  eyes:['plain','glowing','glasses',],
+  lower:['plain','bandana','bane_mask',],
+}
 
+
+function randomWardrobe(wardrobe){
+  /* Expects an object structured like the WARDROBE const above.
+   * Returns a an array of indices, randomly selecting items from that object.*/
+  let result = {};
+  let keys = Object.keys(wardrobe);
+  keys.forEach( key => {
+    result[key] = Math.floor(Math.random() * wardrobe[key].length);
+  });
+  return result;
+}
 
 function arrayOfRands(min = 0, max = 10, length = 1) {
   /* Return an array of size length of random ints */
@@ -18,44 +31,31 @@ function arrayOfRands(min = 0, max = 10, length = 1) {
 class nonPlayableCharacter { 
 
   constructor() {
-    this.name = 'blah'; // Get these in a better way later
-    this.barcode='test'; // Even NPCs must be able to be identified somehow 
-    this.tweet = 'Muh feelz';
-    
-    // Physical attributes to render with image 
-    // Using two magic numbers 3 for now since my arrays are that length and I
-    // have three arrays;
-    // ...must find a better solution later
-    let foo = arrayOfRands(0, 3, 3);
-    this.portrait = {
-      head:HEAD[foo[0]],
-      eyes:EYES[foo[1]],
-      lower:LOWER[foo[2]],
 
-    }
+    this.portrait = {}
+    let myWardrobe = randomWardrobe(WARDROBE);
+    let myKeys = Object.keys(myWardrobe);
+    console.log(`myW: ${toString(myWardrobe)}, myK ${myKeys}`);
+
+    myKeys.forEach( key => {
+      this.portrait[key] = WARDROBE[key][myWardrobe[key]]
+    });
+
   };
 
   drawNPC() {
-    // Code to draw stuff
-    // <img src='./img/npc.png'> // This needs to be rendered in js
     document.getElementById('profile_pic').innerHTML=
         `<div id="base_npc"><img id='base_npc' src='./img/npc.png'></div>`
       + `<div id="head"><img id='head' src='./img/head/${this.portrait.head}.png'></div>`
       + `<div id="eyes"><img id='eyes' src='./img/eyes/${this.portrait.eyes}.png'></div>`
       + `<div id="lower"><img id='lower' src='./img/lower/${this.portrait.lower}.png'></div>`;
   };
-
-  show() {
-    // Generate appropriate HTML 
-    return(`Stuff to be added; text and things`);
-  };
   
 }
 
 function main() {
-  bar = new nonPlayableCharacter();
-  document.getElementById('intro_text').innerHTML = `<h2>Testing</h2> ${bar.show()}`;
-  bar.drawNPC()
+  wojak = new nonPlayableCharacter();
+  wojak.drawNPC()
 }
 
 main();
